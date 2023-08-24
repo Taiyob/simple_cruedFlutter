48ee7fa465ae05067386e284b0218facb644d0f1
OLI <oli1412001@gmail.com>
2023-08-24 16:07:47 +0600

    first test

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:crued_operation/screen/todo_list.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home:  TodoList(),
+    );
+  }
+}
+
+
+
+
+
+
---
+++ lib/screen/add_page.dart
@@ -0,0 +1,88 @@
+import 'package:crued_operation/services/todo_service.dart';
+import 'package:flutter/material.dart';
+import '../utils/snackbar_helper.dart';
+
+class AddTodoPage extends StatefulWidget {
+  final Map? todo;
+  const AddTodoPage({super.key, this.todo,});
+  @override
+  State<AddTodoPage> createState() => _AddTodoPageState();
+}
+class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  bool isEdit = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    final todo = widget.todo;
+    if(todo != null){
+      isEdit = true;
+      final title = todo['title'];
+      final description = todo['description'];
+      titleController.text = title;
+      descriptionController.text = description;
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(isEdit ? 'Edit Todo' : 'Add Todo'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(20),
+        children: [
+          TextField(
+            controller: titleController,
+            decoration: InputDecoration(hintText: 'Title'),
+          ),
+          SizedBox(height: 20,),
+          TextField(
+            controller: descriptionController,
+            decoration: InputDecoration(hintText: 'Description'),
+            keyboardType: TextInputType.multiline,
+            minLines: 5,
+            maxLines: 8,
+          ),
+          SizedBox(height: 20,),
+          ElevatedButton(onPressed:isEdit ? updateData : submitData, child: Text(isEdit ? 'update' : 'submit')),
+        ],
+      ),
+    );
+  }
+  Future<void> updateData() async{
+      final todo = widget.todo;
+      if(todo == null){
+        print('You can not call updated without todo data');
+        return;
+      }
+      final id = todo['_id'];
+      final isSuccess = await TodoService.updateTodo(id, body);
+      if(isSuccess){
+        showSuccessMessage(context, message:'Updating Success');
+      }else{
+        showErrorMessage(context, message:'Updating Failed');
+      }
+  }
+  Future<void> submitData() async {
+      final isSuccess = await TodoService.addTodo(body);
+      if(isSuccess){
+        titleController.text = '';
+        descriptionController.text = '';
+        showSuccessMessage(context, message:'Creation Success');
+      }else{
+        showErrorMessage(context, message:'Creation Failed');
+      }
+  }
+  Map get body{
+    final title = titleController.text;
+    final description = descriptionController.text;
+    return {
+      'title' : title,
+      'description' : description,
+      'is_completed' : false,
+    };
+  }
+}
